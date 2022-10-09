07b14a99dd85a45eed7a3da04a772203f513829f
Altanim <altanim0404@gmail.com>
2022-10-10 02:23:16 +0800

    10 to 7

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        for (int x = 0; x < 10; x++) {
-            for (int y = 0; y < 10; y++) {
-                if (y < 10 - (x + 1)) {
+        for (int x = 0; x < 7; x++) {
+            for (int y = 0; y < 7; y++) {
+                if (y < 7 - (x + 1)) {
                     System.out.print(" ");
                 } else {
-                    System.out.print("0");
+                    System.out.print("00");
                 }
             }
             System.out.println();
